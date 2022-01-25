2c7b16e9d63b39deef21a98c377bcaed07fadf37
NIMISHA <49507092+manishgangwal@users.noreply.github.com>
2022-01-25 17:42:44 +0530

    Add files via upload

---
+++ Math_quizlogin.js
@@ -0,0 +1,8 @@
+function adduser(){
+    player1 = document.getElementById("player1").value ;
+    player2 = document.getElementById("player2").value ;
+    localStorage.setItem("player1_name",player1);
+    localStorage.setItem("player2_name",player2);
+    
+    window.location ="mathq.html";
+    }
